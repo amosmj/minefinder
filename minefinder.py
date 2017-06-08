3b65132cffc5cdcf8f3b526caf600eef6519c280
amosmj <amosmj@gmail.com>
2017-06-08 11:54:02 -0500

    Add files via upload

---
+++ minefinder.py
@@ -0,0 +1,41 @@
+import random
+import math
+
+def buildGrid(width , height,difficulty):
+    grid = []
+    xRow = [0] * width
+    for y in range(height):
+        grid.append(xRow)    
+    
+    mines = math.floor((width*height/10))
+    minePlace = []
+    for m in range(mines):
+        validMine = False
+        while validMine == False:
+            validMine = True
+            newMineLoc = random.randint(0,width*height)
+            for mp in range(len(minePlace)):
+                if mp == newMineLoc:
+                    validMine = False
+        minePlace.append(newMineLoc)
+    print("mine placed: " ,str(minePlace))
+    for mp in minePlace:
+        x = math.floor(mp/width)
+        y = mp % width
+        print('placment: ',str(mp),', X: ',str(x), ', Y: ',str(y))
+        #print('placement row: ',grid[y])
+        print('placement cell: ', grid[y][x])
+        grid[y][x] = 9
+        for i in grid:
+            print(i)
+    return grid
+
+print('Welcome to MineFinder')
+width = int(input('how wide do you want the grid?'))
+height = int(input('how high do you want the grid?'))
+#difficulty = input('how tough do you want the grid?')
+#may need to sanitize inputs
+field = buildGrid(width, height, 0)
+#for y in field:
+#    print(y)
+    
